da8824b1e25e321523ee5a8990517237beb89b4c
alexblack24 <alex.black@mail.com>
2024-04-21 18:39:28 +0200

    Working version

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 from moviepy.editor import VideoFileClip
 
-# Load the mp4 file
-video = VideoFileClip("monsters.mp4")
-
-# Extract audio from video
-video.audio.write_audiofile(filename="monstars.mp3", bitrate="160k", verbose=False)
+def convert_video_to_audio(file_path:str):
+    # Load the mp4 file
+    video = VideoFileClip(file_path)
+    # Extract audio from video
+    video.audio.write_audiofile(filename="./audio/audio.mp3", bitrate="160k", verbose=False)
--- yt.py
+++ yt.py
@@ -1,9 +1,16 @@
 from pytube import YouTube
+from converter import convert_video_to_audio
 
-# AUDIO 
-# stream  = YouTube('https://music.youtube.com/watch?v=YWEUzzHb7MU').streams.filter(only_audio=True).order_by('abr').desc()
-stream  = YouTube('https://music.youtube.com/watch?v=YWEUzzHb7MU').streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
-video = stream.first()
-path = video.download(output_path="./downloads/", filename='monstares.mp4' )
-path = path.replace("\\./", "\\")
-path = path.replace("\\", "/")
+def get_video_from_tube(url='https://music.youtube.com/watch?v=YWEUzzHb7MU')->str:
+    # AUDIO 
+    # stream  = YouTube('https://music.youtube.com/watch?v=YWEUzzHb7MU').streams.filter(only_audio=True).order_by('abr').desc()
+    # VIDEO
+    stream  = YouTube(url).streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+    video = stream.first()
+    path = video.download(output_path="./downloads/")
+    path = path.replace("\\./", "\\")
+    path = path.replace("\\", "/")
+    return path
+
+path = get_video_from_tube()
+convert_video_to_audio(path)
